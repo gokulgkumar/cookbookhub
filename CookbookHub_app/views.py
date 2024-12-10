3cd8e48c9b30c937c27d86921b949d56e327585c
Gokul G Kumar <48852363+gokulgkumar@users.noreply.github.com>
2024-12-10 01:30:47 +0000

    Update views.py

--- CookbookHub_app/views.py
+++ CookbookHub_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect  # type: ignore
 from .forms import *
 from django.contrib import messages  # type: ignore
 from django.contrib.auth import login, logout, authenticate  # type: ignore
-from .models import *
+from .models import Recipes, ManageUser,UserAddData, Subscriptions
 from django.contrib.auth import get_user_model  # type: ignore
 from django.contrib.auth.decorators import login_required  # type: ignore
 from django.db.models import Q  # type: ignore
